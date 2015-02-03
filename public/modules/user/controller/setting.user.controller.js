629ab774b1b8acc3638563b350fbf9e025dd8d65
munzir <munzir.suliman@outlook.com>
2015-02-03 06:17:47 +0400

    fix git ignore
    fix git ignore

--- public/modules/user/controller/setting.user.controller.js
+++ public/modules/user/controller/setting.user.controller.js
@@ -2,7 +2,6 @@
 
 angular.module('userModule').controller('settingUserController', ['$scope', '$location', 'connectUserFactory', 'registerUserConfigFactory', '$state', function ($scope, $location, connectUserFactory, registerUserConfigFactory, $state) {
 	$scope.userInfo = registerUserConfigFactory.getUser();
-
 	$scope.removeAccount = function () {
 		$location.path('/profile/remove');
 	}
