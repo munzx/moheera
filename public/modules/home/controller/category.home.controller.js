0637084eb2a0d8cf1a4d8965249cc2a9b7ebd78a
munzir <munzir.suliman@outlook.com>
2015-02-08 04:32:44 +0400

    remove space
    remove space

--- public/modules/home/controller/category.home.controller.js
+++ public/modules/home/controller/category.home.controller.js
@@ -3,10 +3,8 @@
 angular.module('homeModule').controller('categoryHomeController', ['$scope', 'connectProductFactory', '$stateParams', function ($scope, connectProductFactory, $stateParams) {
 	connectProductFactory.query({action: 'category', getByName: $stateParams.name}, function (response) {
 		$scope.result = response;
-
 		if($scope.result.length){
 			$scope.categoryBanner = 'public/modules/home/img/category/banner/' + $stateParams.name + '.jpg';
 		}
-
 	});
 }]);
