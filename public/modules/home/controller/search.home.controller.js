2d206c2aca45bfcc0f4347c1665f86b8983e6366
munzir <munzir.suliman@outlook.com>
2015-02-09 06:20:37 +0400

    test
    test

--- public/modules/home/controller/search.home.controller.js
+++ public/modules/home/controller/search.home.controller.js
@@ -1,5 +1,50 @@
 'use strict';
 
 angular.module('homeModule').controller('searchHomeController', ['$scope', function ($scope) {
-	
+
+	Caman('#here', function () {
+		this.crop(400, 400);
+		this.resize({
+			width: 400,
+			height: 400
+		});
+	});
+
+	$scope.reset = function () {
+		Caman('#here', function () {
+			this.revert();
+		});
+	}
+
+	$scope.one = function () {
+		Caman('#here', function () {
+			this.revert();
+			this.crossProcess().render(function () {
+				console.log(this.toBase64());
+			});
+		});	
+	}
+
+	$scope.two = function () {
+		Caman('#here', function () {
+			this.revert();
+			this.vintage().render();
+		});	
+	}
+
+	$scope.three = function () {
+		Caman('#here', function () {
+			this.revert();
+			this.lomo().render();
+		});	
+	}
+
+	$scope.four = function () {
+		Caman('#here', function () {
+			this.revert();
+			this.clarity().render();
+		});	
+	}
+
+
 }]);
