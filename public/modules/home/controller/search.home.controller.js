5f21a2a25b47c0cb2187c046a2cf3aae4e039676
munzir <munzir.suliman@outlook.com>
2015-02-09 06:24:15 +0400

    test
    test

--- public/modules/home/controller/search.home.controller.js
+++ public/modules/home/controller/search.home.controller.js
@@ -2,49 +2,49 @@
 
 angular.module('homeModule').controller('searchHomeController', ['$scope', function ($scope) {
 
-	Caman('#here', function () {
-		this.crop(400, 400);
-		this.resize({
-			width: 400,
-			height: 400
-		});
-	});
+	// Caman('#here', function () {
+	// 	this.crop(400, 400);
+	// 	this.resize({
+	// 		width: 400,
+	// 		height: 400
+	// 	});
+	// });
 
-	$scope.reset = function () {
-		Caman('#here', function () {
-			this.revert();
-		});
-	}
+	// $scope.reset = function () {
+	// 	Caman('#here', function () {
+	// 		this.revert();
+	// 	});
+	// }
 
-	$scope.one = function () {
-		Caman('#here', function () {
-			this.revert();
-			this.crossProcess().render(function () {
-				console.log(this.toBase64());
-			});
-		});	
-	}
+	// $scope.one = function () {
+	// 	Caman('#here', function () {
+	// 		this.revert();
+	// 		this.crossProcess().render(function () {
+	// 			console.log(this.toBase64());
+	// 		});
+	// 	});	
+	// }
 
-	$scope.two = function () {
-		Caman('#here', function () {
-			this.revert();
-			this.vintage().render();
-		});	
-	}
+	// $scope.two = function () {
+	// 	Caman('#here', function () {
+	// 		this.revert();
+	// 		this.vintage().render();
+	// 	});	
+	// }
 
-	$scope.three = function () {
-		Caman('#here', function () {
-			this.revert();
-			this.lomo().render();
-		});	
-	}
+	// $scope.three = function () {
+	// 	Caman('#here', function () {
+	// 		this.revert();
+	// 		this.lomo().render();
+	// 	});	
+	// }
 
-	$scope.four = function () {
-		Caman('#here', function () {
-			this.revert();
-			this.clarity().render();
-		});	
-	}
+	// $scope.four = function () {
+	// 	Caman('#here', function () {
+	// 		this.revert();
+	// 		this.clarity().render();
+	// 	});	
+	// }
 
 
 }]);
