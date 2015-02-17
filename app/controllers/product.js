941ab31aa0c1393789534af8a33ee168b422751f
munzir <munzir.suliman@outlook.com>
2015-02-17 12:51:00 +0400

    test
    test

--- app/controllers/product.js
+++ app/controllers/product.js
@@ -151,7 +151,6 @@ module.exports.getByName = function(req, res){
 module.exports.update = function(req, res){
 	var base64Data = "",
 	dest = 'public/uploads/',
-
 	//create object to hold all of the recieved product data
 	formData = {};
 
