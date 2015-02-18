ea19650734bfed8102e9c03fd82d961841806f6a
munzir <munzir.suliman@outlook.com>
2015-02-18 08:31:46 +0400

    show category banner always
    show category banner always even if the category is empty

--- public/modules/home/controller/category.home.controller.js
+++ public/modules/home/controller/category.home.controller.js
@@ -3,8 +3,6 @@
 angular.module('homeModule').controller('categoryHomeController', ['$scope', 'connectProductFactory', '$stateParams', function ($scope, connectProductFactory, $stateParams) {
 	connectProductFactory.query({action: 'category', getByName: $stateParams.name}, function (response) {
 		$scope.result = response;
-		if($scope.result.length){
-			$scope.categoryBanner = 'public/modules/home/img/category/banner/' + $stateParams.name + '.jpg';
-		}
+		$scope.categoryBanner = 'public/modules/home/img/category/banner/' + $stateParams.name + '.jpg';
 	});
 }]);
