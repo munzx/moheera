465937cdc40a9a8a81961828ed882d8fdbc170a5
munzir <munzir.suliman@outlook.com>
2015-03-30 05:58:46 +0400

    test sending email for each new contact
    test sending email for each new contact

--- app/controllers/cms.js
+++ app/controllers/cms.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose'),
 	_ = require('lodash'),
 	errorHandler = require('./error'),
+	email = require('../config/email/config.email.js'),
 	contacts = require('../models/contact');
 
 
@@ -15,8 +16,10 @@ module.exports.contact = function (req, res) {
 
 	contact.save(function (err, newContact) {
 		if(err){
+			//send me and email
 			res.status(500).jsonp({message: errorHandler.getErrorMessage(err)});
 		} else {
+			email.sendEmail(['munzir.suliman@outlook.com']);
 			res.status(200).jsonp('Contact has been sent successfully');
 		}
 	});
