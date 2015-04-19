837362e24890d25c9d3b4c5b88edeff75b21f3af
munzir <munzir.suliman@outlook.com>
2015-04-19 18:37:05 +0400

    fix bug
    fix bug in twitter log in

--- app/config/routes.js
+++ app/config/routes.js
@@ -95,29 +95,26 @@ module.exports = function (app, express) {
 	app.get('/auth/twitter/callback', function (req, res, next) {
 		passport.authenticate('twitter', function (err, account) {
 			if(err.length){
-				//res.redirect('/');
-				res.status(200).json(err);
+				res.redirect('/');
 			} else {
 				if(err.length){
-					res.status(200).json('Bism Allah , here is 1');
-					//res.redirect('/');
+					res.redirect('/');
 				} else {
-					res.status(200).json('Bism Allah , here is 2');
-					// if(req.user && account.user.length == 0){
-					// 	res.redirect('/api/v1/account/link/' + account._id);
-					// } else if(req.user && account.user.length > 0){
-					// 	res.render('../public/modules/config/view/index', {
-					// 		isAuthenticated: req.isAuthenticated(),
-					// 		userInfo: req.user,
-					// 		query: {page: '/profile/profile/social', key: 'error', value: 'account already linked'}
-					// 	});;
-					// } else if(!req.user && account.user.length > 0){
-					// 	req.login(account.user[0], function (err) {
-					// 		res.redirect('/');
-					// 	});
-					// } else {
-					// 	res.redirect('/provider/' + account._id);
-					// }
+					if(req.user && account.user.length == 0){
+						res.redirect('/api/v1/account/link/' + account._id);
+					} else if(req.user && account.user.length > 0){
+						res.render('../public/modules/config/view/index', {
+							isAuthenticated: req.isAuthenticated(),
+							userInfo: req.user,
+							query: {page: '/profile/profile/social', key: 'error', value: 'account already linked'}
+						});;
+					} else if(!req.user && account.user.length > 0){
+						req.login(account.user[0], function (err) {
+							res.redirect('/');
+						});
+					} else {
+						res.redirect('/provider/' + account._id);
+					}
 				}
 			}
 		})(req, res, next);
