40397426c5799217eea01f3b99977420c5d78895
munzir <munzir.suliman@outlook.com>
2015-04-19 18:26:09 +0400

    test
    test

--- app/config/routes.js
+++ app/config/routes.js
@@ -96,25 +96,28 @@ module.exports = function (app, express) {
 		passport.authenticate('twitter', function (err, account) {
 			if(err){
 				//res.redirect('/');
+				res.status(200).json('Bism Allah , here is 0');
 			} else {
 				if(err){
+					res.status(200).json('Bism Allah , here is 1');
 					//res.redirect('/');
 				} else {
-					if(req.user && account.user.length == 0){
-						res.redirect('/api/v1/account/link/' + account._id);
-					} else if(req.user && account.user.length > 0){
-						res.render('../public/modules/config/view/index', {
-							isAuthenticated: req.isAuthenticated(),
-							userInfo: req.user,
-							query: {page: '/profile/profile/social', key: 'error', value: 'account already linked'}
-						});;
-					} else if(!req.user && account.user.length > 0){
-						req.login(account.user[0], function (err) {
-							res.redirect('/');
-						});
-					} else {
-						res.redirect('/provider/' + account._id);
-					}
+					res.status(200).json('Bism Allah , here is 2');
+					// if(req.user && account.user.length == 0){
+					// 	res.redirect('/api/v1/account/link/' + account._id);
+					// } else if(req.user && account.user.length > 0){
+					// 	res.render('../public/modules/config/view/index', {
+					// 		isAuthenticated: req.isAuthenticated(),
+					// 		userInfo: req.user,
+					// 		query: {page: '/profile/profile/social', key: 'error', value: 'account already linked'}
+					// 	});;
+					// } else if(!req.user && account.user.length > 0){
+					// 	req.login(account.user[0], function (err) {
+					// 		res.redirect('/');
+					// 	});
+					// } else {
+					// 	res.redirect('/provider/' + account._id);
+					// }
 				}
 			}
 		})(req, res, next);
