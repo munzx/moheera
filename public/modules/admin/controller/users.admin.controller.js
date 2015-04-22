bd62ab0bf40f000039d8fe820bf53ddfeb1f7894
munzir <munzir.suliman@outlook.com>
2015-04-22 05:15:48 +0400

    remove console log
    remove console log

--- public/modules/admin/controller/users.admin.controller.js
+++ public/modules/admin/controller/users.admin.controller.js
@@ -4,7 +4,6 @@ angular.module('adminModule').controller('usersAdminController', ['$scope', 'con
 
 	connectAdminFactory.get({page: 'users'}, function (response) {
 		$scope.users = response.users;
-		console.log(response.users);
 	});
 
 }]);
