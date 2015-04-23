309e34bb666cb4889200063d52dbfd14c45c4679
munzir <munzir.suliman@outlook.com>
2015-04-24 02:13:33 +0400

    test
    test

--- public/modules/admin/controller/orders.admin.controller.js
+++ public/modules/admin/controller/orders.admin.controller.js
@@ -1,5 +1,7 @@
 'use strict';
 
 angular.module('adminModule').controller('ordersAdminController', ['$scope', 'connectAdminFactory', function ($scope, connectAdminFactory) {
-	
+	connectAdminFactory.get({page: 'orders'}, function (response) {
+		console.log(response);
+	});
 }]);
