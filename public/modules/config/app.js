d88c30fa5698deb0b15553da5d8a4e0044c546fb
munzir <munzir.suliman@outlook.com>
2015-05-28 02:08:30 +0400

    enable cache
    enable cache

--- public/modules/config/app.js
+++ public/modules/config/app.js
@@ -22,126 +22,126 @@ moheera.config(['$urlRouterProvider', '$stateProvider', '$locationProvider', 'Ch
 			url: '/notfound',
 			templateUrl: 'public/modules/config/view/notfound.config.view.html',
 			controller: 'errorConfigController',
-			cache: false
+			cache: true
 		})
 		.state('home', {
 			url: '/',
 			templateUrl: 'public/modules/home/view/index.home.view.html',
 			controller: 'indexHomeController',
-			cache: false
+			cache: true
 		})
 		.state('image', {
 			url: '/service/image',
 			templateUrl: 'public/modules/home/view/image.home.view.html',
 			controller: 'imageHomeController',
-			cache: false
+			cache: true
 		})
 		.state('search', {
 			url: '/search',
 			templateUrl: 'public/modules/home/view/search.home.view.html',
 			controller: 'searchHomeController',
-			cache: false
+			cache: true
 		})
 		.state('admin', {
 			url: '/admin',
 			templateUrl: 'public/modules/admin/view/index.admin.view.html',
 			controller: 'indexAdminController',
-			cache: false
+			cache: true
 		})
 		.state('admin.users', {
 			url: '/users',
 			templateUrl: 'public/modules/admin/view/users.admin.view.html',
 			controller: 'usersAdminController',
-			cache: false
+			cache: true
 		})
 		.state('admin.reports', {
 			url: '/reports',
 			templateUrl: 'public/modules/admin/view/reports.admin.view.html',
 			controller: 'reportsAdminController',
-			cache: false
+			cache: true
 		})
 		.state('admin.messages', {
 			url: '/messages',
 			templateUrl: 'public/modules/admin/view/messages.admin.view.html',
 			controller: 'messagessAdminController',
-			cache: false
+			cache: true
 		})
 		.state('admin.products', {
 			url: '/products',
 			templateUrl: 'public/modules/admin/view/products.admin.view.html',
 			controller: 'productsAdminController',
-			cache: false
+			cache: true
 		})
 		.state('admin.orders', {
 			url: '/orders',
 			templateUrl: 'public/modules/admin/view/orders.admin.view.html',
 			controller: 'ordersAdminController',
-			cache: false
+			cache: true
 		})		
 		.state('search.user', {
 			url: '/user/:name',
 			templateUrl: 'public/modules/home/view/search.user.home.view.html',
 			controller: 'searchUserHomeController',
-			cache: false
+			cache: true
 		})
 		.state('search.product', {
 			url: '/product/:name',
 			templateUrl: 'public/modules/home/view/search.product.home.view.html',
 			controller: 'searchProductHomeController',
-			cache: false
+			cache: true
 		})
 		.state('category', {
 			url: '/category/:name',
 			templateUrl: 'public/modules/home/view/category.home.view.html',
 			controller: 'categoryHomeController',
-			cache: false
+			cache: true
 		})
 		.state('contact', {
 			url: '/contact',
 			templateUrl: 'public/modules/home/view/contact.home.view.html',
 			controller: 'indexHomeController',
-			cache: false
+			cache: true
 		})
 		.state('about', {
 			url: '/about',
 			templateUrl: 'public/modules/home/view/about.home.view.html',
 			controller: 'indexHomeController',
-			cache: false
+			cache: true
 		})
 		.state('whyMoheera', {
 			url: '/why_moheera',
 			templateUrl: 'public/modules/home/view/why.moheera.home.view.html',
 			controller: 'indexHomeController',
-			cache: false
+			cache: true
 		})
 		.state('signin', {
 			url: '/signin',
 			templateUrl: 'public/modules/auth/view/signin.auth.view.html',
 			controller: 'signinAuthController',
-			cache: false
+			cache: true
 		})
 		.state('signup', {
 			url: '/signup',
 			templateUrl: 'public/modules/auth/view/signup.auth.view.html',
 			controller: 'signupAuthController',
-			cache: false
+			cache: true
 		})
 		.state('providerSignIn', {
 			url: '/signin/provider/:id',
 			templateUrl: 'public/modules/auth/view/provider.signin.auth.view.html',
 			controller: 'signInProviderAuthController',
-			cache: false
+			cache: true
 		})
 		.state('signout', {
 			url: '/signout',
 			controller: 'signoutAuthController',
-			cache: false
+			cache: true
 		})
 		.state('profile', {
 			url: '/profile',
 			templateUrl: 'public/modules/user/view/profile.user.view.html',
 			controller: 'profileUserControlller',
-			cache: false
+			cache: true
 		})
 		.state('profile.category', {
 			url: '/product/category/:category',
@@ -152,91 +152,91 @@ moheera.config(['$urlRouterProvider', '$stateProvider', '$locationProvider', 'Ch
 			url: '/remove',
 			templateUrl: 'public/modules/user/view/remove.account.user.view.html',
 			controller: 'removeAccountUserController',
-			cache: false
+			cache: true
 		})
 		.state('profile.setting', {
 			url:'/setting',
 			templateUrl: 'public/modules/user/view/setting.user.view.html',
 			controller: 'settingUserController',
-			cache: false
+			cache: true
 		})
 		.state('profile.social', {
 			url: '/profile/social',
 			templateUrl: 'public/modules/user/view/social.user.view.html',
 			controller: 'socialUserControlller',
-			cache: false
+			cache: true
 		})
 		.state('profile.userPassword', {
 			url: '/account/password',
 			templateUrl: 'public/modules/user/view/change.password.user.view.html',
 			controller: 'changePasswordUserController',
-			cache: false
+			cache: true
 		})
 		.state('profile.orderDone', {
 			url: '/order/done',
 			templateUrl: 'public/modules/order/view/done.order.view.html',
 			controller: 'allOrderController',
-			cache: false
+			cache: true
 		})
 		.state('profile.orderCreate', {
 			url: '/order/create',
 			templateUrl: 'public/modules/order/view/create.order.view.html',
 			controller: 'createOrderController',
-			cache: false
+			cache: true
 		})
 		.state('profile.singleOrder', {
 			url: '/product/:productId/order/:id/',
 			templateUrl: 'public/modules/order/view/single.order.view.html',
 			controller: 'singleOrderController',
-			cache: false
+			cache: true
 		})
 		.state('profile.order', {
 			url: '/order',
 			templateUrl: 'public/modules/order/view/all.order.view.html',
 			controller: 'allOrderController',
-			cache: false
+			cache: true
 		})
 		.state('profile.addProduct', {
 			url: '/product/add',
 			templateUrl: 'public/modules/product/view/add.product.view.html',
 			controller: 'addProductController',
-			cache: false
+			cache: true
 		})
 		.state('profile.editProduct', {
 			url: '/product/:name/edit',
 			templateUrl: 'public/modules/product/view/edit.product.view.html',
 			controller: 'editProductController',
-			cache: false
+			cache: true
 		})
 		.state('profile.singleProduct', {
 			url: '/product/:name',
 			templateUrl: 'public/modules/product/view/single.product.view.html',
 			controller: 'singleProductController',
-			cache: false
+			cache: true
 		})
 		.state('profile.cart', {
 			url: '/cart',
 			templateUrl: 'public/modules/cart/view/all.cart.view.html',
 			controller: 'allCartController',
-			cache: false
+			cache: true
 		})
 		.state('user', {
 			url: '/:userName',
 			templateUrl: 'public/modules/user/view/other.user.view.html',
 			controller: 'otherUserControlller',
-			cache: false
+			cache: true
 		})
 		.state('user.singleProduct', {
 			url: '/:productName',
 			templateUrl: 'public/modules/product/view/other.single.product.view.html',
 			controller: 'otherSingleProductControlller',
-			cache: false
+			cache: true
 		})
 		.state('user.category', {
 			url: '/product/category/:category',
 			templateUrl: 'public/modules/user/view/other.category.user.view.html',
 			controller: 'categoryUserController',
-			cache: false
+			cache: true
 		});
 		$locationProvider.html5Mode(true).hashPrefix('!');
 }])
