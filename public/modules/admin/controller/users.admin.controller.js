aaf6610d6a757fa90153ab1c42c2f6c392d3d019
munzir <munzir.suliman@outlook.com>
2015-05-30 22:06:27 +0400

    test
    test

--- public/modules/admin/controller/users.admin.controller.js
+++ public/modules/admin/controller/users.admin.controller.js
@@ -1,12 +1,21 @@
 'use strict';
 
 angular.module('adminModule').controller('usersAdminController', ['$scope', 'connectAdminFactory', function ($scope, connectAdminFactory) {
-	var limit = 16;
+	var limit = 20,
+		skip = 0;
+
+	$scope.listLimit = 0;
+	$scope.users = [];
 
 	$scope.addMore = function () {
-		connectAdminFactory.get({page: 'users', "limit": limit}, function (response) {
-			$scope.users = response.users;
-			limit++;
+		connectAdminFactory.get({page: 'users', "limit": limit, "skip": skip}, function (response) {
+			if(response.users.length > 0){
+				for(var i=0; i < response.users.length; i++){
+					$scope.users.push(response.users[i]);
+				}
+				skip+= limit;
+				$scope.listLimit = skip;
+			}
 		});
 	}
 
