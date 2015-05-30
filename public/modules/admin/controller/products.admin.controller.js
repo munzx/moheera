6e8a8a4f5e6e7a1f91103456c8149b464c8f61b3
munzir <munzir.suliman@outlook.com>
2015-05-30 21:41:05 +0400

    test
    test

--- public/modules/admin/controller/products.admin.controller.js
+++ public/modules/admin/controller/products.admin.controller.js
@@ -1,12 +1,19 @@
 'use strict';
 
 angular.module('adminModule').controller('productsAdminController', ['$scope', 'connectAdminFactory', function ($scope, connectAdminFactory) {
-	var limit = 16;
+	var limit = 2,
+		skip = 0;
+
+	$scope.products = [];
 
 	$scope.addMore = function () {
-		connectAdminFactory.get({page: 'products', "limit": limit}, function (response) {
-			$scope.products = response.products;
-			limit++;
+		connectAdminFactory.get({page: 'products', "limit": limit, "skip": skip}, function (response) {
+			if(response.products.length > 0){
+				for(var i=0; i < response.products.length; i++){
+					$scope.products.push(response.products[i]);
+				}
+				skip+= limit;
+			}
 		});
 	}
 
