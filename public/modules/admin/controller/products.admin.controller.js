59ede4b855935b83f4c46c0c728525e7bbf3a2d2
munzir <munzir.suliman@outlook.com>
2015-05-31 01:21:23 +0400

    fix bugs
    fix bugs

--- public/modules/admin/controller/products.admin.controller.js
+++ public/modules/admin/controller/products.admin.controller.js
@@ -6,10 +6,10 @@ angular.module('adminModule').controller('productsAdminController', ['$scope', '
 	$scope.skip = 0;
 	$scope.products = [];
 
-	$scope.addMore = function () {
+	$scope.addMoreProducts = function () {
 		connectAdminFactory.get({page: 'products', "limit": limit, "skip": $scope.skip}, function (response) {
 			if(response.products.length > 0){
-				for(var i=0; i < response.products.length; i++){
+				for(var i=1; i < response.products.length; i++){
 					$scope.products.push(response.products[i]);
 				}
 				$scope.skip+= limit;
--- public/modules/admin/controller/users.admin.controller.js
+++ public/modules/admin/controller/users.admin.controller.js
@@ -1,18 +1,18 @@
 'use strict';
 
 angular.module('adminModule').controller('usersAdminController', ['$scope', 'connectAdminFactory', function ($scope, connectAdminFactory) {
-	var limit = 20,
-		$scope.skip = 0;
+	var limit = 20;
+		
+	$scope.skip = 0;
+	$scope.allUsers = [];
 
-	$scope.users = [];
-
-	$scope.addMore = function () {
+	$scope.addMoreUsers = function () {
 		connectAdminFactory.get({page: 'users', "limit": limit, "skip": $scope.skip}, function (response) {
 			if(response.users.length > 0){
-				for(var i=0; i < response.users.length; i++){
-					$scope.users.push(response.users[i]);
+				for(var i=1; i < response.users.length; i++){
+					$scope.allUsers.push(response.users[i]);
 				}
-				$scope.skip+= limit;
+				$scope.skip += limit;
 			}
 		});
 	}
