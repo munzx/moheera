ed853046ba87a3082897f9b5cb434bb2d85a4cde
munzir <munzir.suliman@outlook.com>
2015-06-10 17:18:24 +0400

    test
    test

--- public/modules/admin/controller/users.admin.controller.js
+++ public/modules/admin/controller/users.admin.controller.js
@@ -8,11 +8,11 @@ angular.module('adminModule').controller('usersAdminController', ['$scope', 'con
 	$scope.busy = false;
 
 	$scope.addMoreUsers = function () {
-		$scope.busy = true;
 		connectAdminFactory.get({page: 'users', "limit": limit, "skip": $scope.skipUsers}, function (response) {
 			if(response.users.length > 0){
 				for(var i=0; i < response.users.length; i++){
 					$scope.allUsers.push(response.users[i]);
+					$scope.busy = true;
 				}
 			}
 		});
