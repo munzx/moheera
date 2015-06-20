586055930ecdf09ef398f2eecfbc6ebdbf207d65
munzir <munzir.suliman@outlook.com>
2015-06-20 15:25:43 +0400

    test
    test

--- public/modules/home/controller/category.home.controller.js
+++ public/modules/home/controller/category.home.controller.js
@@ -2,23 +2,23 @@
 
 angular.module('homeModule').controller('categoryHomeController', ['$scope', 'connectProductFactory', '$stateParams', 'registerUserConfigFactory',function ($scope, connectProductFactory, $stateParams, registerUserConfigFactory) {
 	var limit = 25;
-	$scope.skipProducts = 0;
+	var skipProducts = 0;
 	$scope.products = [];
 
 	$scope.userInfo = registerUserConfigFactory.getUser();
 	$scope.categoryBanner = 'public/modules/home/img/category/banner/' + $stateParams.name + '.jpg';
 
 	$scope.getByCountry = function () {
-		$scope.skipProducts = 0;
-		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip": $scope.skipProducts}, function (response) {
+		skipProducts = 0;
+		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip":	skipProducts}, function (response) {
 			$scope.products = response;
-			$scope.skipProducts = limit;
+			skipProducts = limit;
 		});
 	}
 
 	$scope.addMoreProducts = function () {
-		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip": $scope.skipProducts}, function (response) {
-			console.log(limit);
+		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip":	skipProducts}, function (response) {
+			console.log(skipProducts);
 			if(response.length > 0){
 				$scope.allResult = $scope.allResult || angular.copy(response);
 				for(var i=0; i < response.length; i++){
@@ -28,7 +28,7 @@ angular.module('homeModule').controller('categoryHomeController', ['$scope', 'co
 			}
 			$scope.busy = false;
 		});
-		$scope.skipProducts+= limit;
+		skipProducts+= limit;
 	}
 
 }]);
