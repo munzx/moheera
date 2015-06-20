2bfa424e69d1685e52978299bc6f7131874d3811
munzir <munzir.suliman@outlook.com>
2015-06-20 05:13:38 +0400

    test
    test

--- public/modules/home/controller/category.home.controller.js
+++ public/modules/home/controller/category.home.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('homeModule').controller('categoryHomeController', ['$scope', 'connectProductFactory', '$stateParams', 'registerUserConfigFactory',function ($scope, connectProductFactory, $stateParams, registerUserConfigFactory) {
-	var limit = 20;
+	var limit = 25;
 	$scope.skipProducts = 0;
 	$scope.products = [];
 
@@ -17,17 +17,18 @@ angular.module('homeModule').controller('categoryHomeController', ['$scope', 'co
 	}
 
 	$scope.addMoreProducts = function () {
+		$scope.busy = true;
 		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip": $scope.skipProducts}, function (response) {
 			if(response.length > 0){
 				$scope.allResult = $scope.allResult || angular.copy(response);
 				for(var i=0; i < response.length; i++){
 					$scope.products.push(response[i]);
-					$scope.busy = true;
 				}
 			}
-			$scope.busy = false;
-			$scope.skipProducts+= limit;
 		});
+		$scope.skipProducts+= limit;
+		$scope.busy = false;
+		console.log($scope.skipProducts);
 	}
 
 }]);
