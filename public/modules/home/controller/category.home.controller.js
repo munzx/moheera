d44a081f7b90b569c56a6199ee2d55a62fb0cd96
munzir <munzir.suliman@outlook.com>
2015-06-20 15:56:32 +0400

    temporary fix
    temporary fix

--- public/modules/home/controller/category.home.controller.js
+++ public/modules/home/controller/category.home.controller.js
@@ -1,34 +1,44 @@
 'use strict';
 
 angular.module('homeModule').controller('categoryHomeController', ['$scope', 'connectProductFactory', '$stateParams', 'registerUserConfigFactory',function ($scope, connectProductFactory, $stateParams, registerUserConfigFactory) {
-	var limit = 20;
-	$scope.skipProducts = 0;
-	$scope.products = [];
+	// var limit = 20;
+	// $scope.skipProducts = 0;
+	// $scope.products = [];
 
 	$scope.userInfo = registerUserConfigFactory.getUser();
 	$scope.categoryBanner = 'public/modules/home/img/category/banner/' + $stateParams.name + '.jpg';
 
 	$scope.getByCountry = function () {
-		$scope.skipProducts = 0;
-		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip": $scope.skipProducts}, function (response) {
+		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null'}, function (response) {
 			$scope.products = response;
-			$scope.skipProducts = limit;
 		});
 	}
 
+	connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null'}, function (response) {
+		$scope.products = response;
+		$scope.allResult = $scope.allResult || angular.copy(response);
+	});
 
-	$scope.addMoreProducts = function () {
-		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip": $scope.skipProducts}, function (response) {
-			if(response.length > 0){
-				for(var i=0; i < response.length; i++){
-					$scope.products.push(response[i]);
-					$scope.busy = true;
-				}
-			}
-			$scope.busy = false;
-		});
-		$scope.skipProducts+= limit;
-	}
+	// $scope.getByCountry = function () {
+	// 	$scope.skipProducts = 0;
+	// 	connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip": $scope.skipProducts}, function (response) {
+	// 		$scope.products = response;
+	// 		$scope.skipProducts = limit;
+	// 	});
+	// }
 
+	// $scope.addMoreProducts = function () {
+	// 	connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip": $scope.skipProducts}, function (response) {
+	// 		if(response.length > 0){
+	// 			$scope.allResult = $scope.allResult || angular.copy(response);
+	// 			for(var i=0; i < response.length; i++){
+	// 				$scope.products.push(response[i]);
+	// 				$scope.busy = true;
+	// 			}
+	// 		}
+	// 		$scope.busy = false;
+	// 	});
+	// 	$scope.skipProducts+= limit;
+	// }
 
 }]);
