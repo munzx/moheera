50484e9eef84312c1eda1274bf993f0de418f0fc
munzir <munzir.suliman@outlook.com>
2015-06-20 15:31:07 +0400

    test
    test

--- public/modules/home/controller/category.home.controller.js
+++ public/modules/home/controller/category.home.controller.js
@@ -1,26 +1,25 @@
 'use strict';
 
 angular.module('homeModule').controller('categoryHomeController', ['$scope', 'connectProductFactory', '$stateParams', 'registerUserConfigFactory',function ($scope, connectProductFactory, $stateParams, registerUserConfigFactory) {
-	var limit = 25;
-	var skipProducts = 0;
+	var limit = 20;
+	$scope.skipProducts = 0;
 	$scope.products = [];
 
 	$scope.userInfo = registerUserConfigFactory.getUser();
 	$scope.categoryBanner = 'public/modules/home/img/category/banner/' + $stateParams.name + '.jpg';
 
 	$scope.getByCountry = function () {
-		skipProducts = 0;
-		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip":	skipProducts}, function (response) {
+		$scope.skipProducts = 0;
+		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip": $scope.skipProducts}, function (response) {
 			$scope.products = response;
-			skipProducts = limit;
+			$scope.skipProducts = limit;
 		});
 	}
 
+
 	$scope.addMoreProducts = function () {
-		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip":	skipProducts}, function (response) {
-			console.log(skipProducts);
+		connectProductFactory.query({action: 'category', getByName: $stateParams.name, countryName: $scope.country || 'null', "limit": limit, "skip": $scope.skipProducts}, function (response) {
 			if(response.length > 0){
-				$scope.allResult = $scope.allResult || angular.copy(response);
 				for(var i=0; i < response.length; i++){
 					$scope.products.push(response[i]);
 					$scope.busy = true;
@@ -28,7 +27,8 @@ angular.module('homeModule').controller('categoryHomeController', ['$scope', 'co
 			}
 			$scope.busy = false;
 		});
-		skipProducts+= limit;
+		$scope.skipProducts+= limit;
 	}
+
 
 }]);
